45164b21c4e2099f80ee0ea9f32e2204680335ed
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-04-18 00:22:45 -0400

    refactor args prompt

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,38 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     println!("Welcome to my cli app!");
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for '{}' in file: {}", config.query, config.filename);
 
-    println!("Searching for '{}' in file: {}", query, file);
+    run(config);
+}
 
-    let content = fs::read_to_string(file).expect("Something went wrong reading the file");
+fn run(config:Config) {
+    let content = fs::read_to_string(config.filename)
+    .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", content);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
